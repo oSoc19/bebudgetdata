82c037f06d09af76fc802233894a5ac1caeeb180
Bert Jehoul <jeborsel@gmail.com>
2019-07-22 05:39:10 +0000

    corrected rda naming

--- R/transformations.R
+++ R/transformations.R
@@ -5,4 +5,4 @@ budgetbygovtype<-nfgovaccount %>%
   select(Value, `Government type`) %>%
   mutate(ValueinBillion=Value/1000) %>%
   mutate(Valueontotal1000=ValueinBillion/235.9764*1000)
-usethis::use_data(govbytype, overwrite = TRUE)
+usethis::use_data(budgetbygovtype, overwrite = TRUE)
