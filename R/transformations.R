61cee08650f7561dadc04932f3f2ba40f3adc04b
Bert Jehoul <jeborsel@gmail.com>
2019-07-22 05:44:02 +0000

    restructure script

--- R/transformations.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-budgetbygovtype<-nfgovaccount %>%
-  filter(Time==2018) %>%
-  filter(Item=='Expenditure') %>%
-  select(Value, `Government type`) %>%
-  mutate(ValueinBillion=Value/1000) %>%
-  mutate(Valueontotal1000=ValueinBillion/235.9764*1000)
-usethis::use_data(budgetbygovtype, overwrite = TRUE)
